2ab08d8ed5bb5179d761e2455fd7ce64d7abba93
eliot charette and william kinzig <eliot@>
2018-06-04 15:56:19 -0700

    Use .foreach to modify sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
-$(function () {
-
-  var array = [];
-
-  $("#formOne").submit(function(event){
-    var item = $("#item").val();
-    array.push(item);
-
-
-    array.sort();
 
 
 
-    $("#story").show();
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var splitSentence = person1Input.split(" ");
+
+    splitSentence.forEach(function(word) {
+      if (word.length >= 3) {
+        $(".person1").append("<li>"+word + "</li>");
+      }
+    })
 
     event.preventDefault();
-  })
-})
+  });
+});
