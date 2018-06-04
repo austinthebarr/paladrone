e3d756928a6c30ab4a63fc2a01076dffbbf6fac8
eliot charette and william kinzig <william@>
2018-06-04 16:08:49 -0700

    Add reverse order

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,20 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var person1Input = $("input#person1").val();
     var splitSentence = person1Input.split(" ");
+    var reverseSentence = splitSentence.reverse();
 
     splitSentence.forEach(function(word) {
-      if (word.length >= 3) {
-        $(".person1").append("<li>"+word + "</li>");
+      if (word.length < 3) {
+        $(".person1").append("<li>"+ word + "</li>");
       }
     })
+
+    reverseSentence.forEach(function(word) {
+      if (word.length < 10) {
+        $(".person1").append("<li>"+ word + "</li>");
+      }
+    })
+
 
     event.preventDefault();
   });
